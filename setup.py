a606601ae2ce95cdfd5cdf542c7b47fe4eb43ac7
William Myers <mwilliammyers@gmail.com>
2018-02-22 14:39:37 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='regexv',
+    version='0.0.1',
+    packages=['regexv'],
+    url='https://github.com/NancyFulda/regexv',
+    license='MIT',
+    author='BYU',
+    author_email='',
+    description='regex + word embedding vectors',
+    install_requires=['numpy', 'scipy']
+)
