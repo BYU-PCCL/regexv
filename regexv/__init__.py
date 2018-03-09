0d27d47a9441cf8f2a114bdd2a87c54ac47520bb
Tyler Etchart <tyler.etchart@gmail.com>
2018-03-09 15:17:26 -0700

    Added expand and compile_file

--- regexv/__init__.py
+++ regexv/__init__.py
@@ -7,32 +7,48 @@ import regexv.utils
 
 vec_master = regexv.utils.vecMaster()
 
+def expand(pattern, delimiters=('<', '>')):
+    for substring in re.finditer(r"(?:{}([^{}]+){})*".format(delimiters[0], delimiters[0], delimiters[1]), pattern):
+        if not substring.group(1):
+            continue
+
+        m = substring.group(1)
+        words = m.split(",")
+        words = [word.strip() for word in words]
+
+        expanded_words = vec_master.neighbor_expansion(words)
+
+        first_word_index = pattern.find(m)
+        last_word_index = first_word_index + len(m)
+
+        expanded_string = pattern[:first_word_index]
+        expanded_string += "|".join(list(expanded_words))
+        expanded_string += pattern[last_word_index:]
+        pattern = expanded_string
+
+    pattern = re.sub(delimiters[0], '(', pattern)
+    pattern = re.sub(delimiters[1], ')', pattern)
+    return pattern
+
+
+def compile_file(from_file, to_file):
+    # read lines from from_file
+    content = from_file.readlines()
+    for line in content:
+
+        # if line is regex, then expand
+        if line[0] != "#" and line[0] != "[":
+            line = expand(line)
+
+        # write to new file
+        to_file.write(line)
+
 
 def wrap(method):
     def fn(*args, **kwargs):
         kwargs.setdefault('delimiters', ('<', '>'))
-        pattern = args[0]
         delimiters = kwargs['delimiters']
-        for substring in re.finditer(r"(?:{}([^{}]+){})*".format(delimiters[0], delimiters[0], delimiters[1]), pattern):
-            if not substring.group(1):
-                continue
-
-            m = substring.group(1)
-            words = m.split(",")
-            words = [word.strip() for word in words]
-
-            expanded_words = vec_master.neighbor_expansion(words)
-
-            first_word_index = pattern.find(m)
-            last_word_index = first_word_index + len(m)
-
-            expanded_string = pattern[:first_word_index]
-            expanded_string += "|".join(list(expanded_words))
-            expanded_string += pattern[last_word_index:]
-            pattern = expanded_string
-
-        pattern = re.sub(delimiters[0], '(', pattern)
-        pattern = re.sub(delimiters[1], ')', pattern)
+        pattern = expand(args[0], delimiters=delimiters)
 
         new_args = list(args)
         new_args[0] = pattern
